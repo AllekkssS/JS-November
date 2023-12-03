2bea8611ba817c6d03508a090f01250dedd92785
AllekkssS <150175703+AllekkssS@users.noreply.github.com>
2023-12-03 16:24:02 +0100

    Update homework7.js

--- Homework7/homework7.js
+++ Homework7/homework7.js
@@ -7,15 +7,17 @@ window.onload = function() {
         {title: "Master and Margarita", author:"Mihail Bulgakov"},
         {title: "Love in Lowercase", author:"Francesc Miralles"},
         {title: "1Q84", author:"Haruki Murakami"},
-        {title: "Demons", author:"Fyodor Dostoevsky"},
-        //{title: "The Da Vinci Code", author:"Dan Brown"}
+        {title: "Demons", author:"Fyodor Dostoevsky"}
     ]
 
     function createTable(objectBooks, caption) {
         let table = document.createElement("table")
+        table.setAttribute("id", "tableBooks")
         let tableCaption = document.createElement("caption")
         tableCaption.innerHTML = caption
         table.appendChild(tableCaption)
+        tableCaption.setAttribute("id", "captionStyle")
+        //document.getElementById("captionStyle").style.marginBottom = "10px"
 
         let keysFromObject = objectBooks[0]
         let mainRow = document.createElement("tr")
@@ -28,20 +30,50 @@ window.onload = function() {
         table.appendChild(mainRow)
 
         for(element of objectBooks){
+            //console.log(element)
             let thirdRow = document.createElement("tr")
             for(key in element){
+                //console.log(key)
                 let content = document.createElement("td")
                 content.innerText = element[key]
+                //console.log(element[key])
                 thirdRow.appendChild(content)
             }
             table.appendChild(thirdRow)
         }
 
         table.setAttribute("border", "5px", "solid")
-        document.body.appendChild(table)
-        
+        document.body.appendChild(table)        
     }
 
     createTable(books,"List of Books")
+}
 
+function addToWishList() {
+    var newTitle = document.getElementById("title")
+    var newAuthor = document.getElementById("author")
+    var enteredTitle = newTitle.value.trim()
+    var enteredAuthor = newAuthor.value.trim()    
+
+    if (enteredTitle, enteredAuthor) {
+        var tableBooks = document.getElementById("tableBooks")
+        var newInfo = document.createElement("tr")
+        tableBooks.appendChild(newInfo) 
+        addedTitle = document.createElement("td")
+        addedTitle.innerText = enteredTitle
+        addedAuthor = document.createElement("td")
+        addedAuthor.innerText = enteredAuthor
+        newInfo.appendChild(addedTitle)
+        newInfo.appendChild(addedAuthor)
+        newTitle.value = ""
+        newAuthor.value = ""     
+    }    
+}
+
+function removeFromWishList() {
+    var tableBooks = document.getElementById("tableBooks")
+    var lastInput = tableBooks.lastElementChild
+    if(lastInput){
+        tableBooks.removeChild(lastInput)
+    }
 }
